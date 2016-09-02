8cbd1f75b902a55f9819079e9fb385c8a9d17339
Davide Bortolami <d@fermiumlabs.com>
2016-09-02 20:10:18 +0200

    only compile changed files, log filenames, added comments, clenup

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,45 @@ var shell = require('gulp-shell');
 var cache = require('gulp-cached');
 var clean = require('gulp-clean');
 var frontMatter = require('gulp-front-matter');
+var changed = require('gulp-changed');
+var print = require('gulp-print');
 
 
-//invoke pandoc and build the pdfs
+// invoke pandoc and build the pdfs
 gulp.task('pdf', function() {
     return gulp.src('_input/*.md', {
             verbose: false
-        })
+            })
+        .pipe(changed('_build', {extension: '.pdf'})) //only changed pdf files
+        .pipe(print())
         .pipe(frontMatter({
       property: 'frontMatter', // property added to file object 
       remove: false // should we remove front-matter header? 
     }))
         .pipe(shell([
             'mkdir -p _build',
-            'pandoc --latex-engine=xelatex --template=' + __dirname + '/template/<%= f(file.frontMatter.template) %>.tex -o _build/<%= f(file.relative) %> _input/<%= file.relative %>'
-        ], {
-            templateData: {
-                f: function(s) {
-                    return ("" + s).split('.md').join('.pdf')
-                }
-            }
-        }))
+            'pandoc --latex-engine=xelatex --template=' + __dirname + '/template/<%= file.frontMatter.template %>.tex -o _build/<%= file.relative.replace(".md", ".pdf") %> _input/<%= file.relative %>'
+        ]))
 })
 
 // compress and optimize the pdf files with ghostscript
 gulp.task('compress', ['pdf'], function() {
     return gulp.src('_build/*.pdf')
+        .pipe(changed('_build/*.pdf')) //only changed pdf files
+        .pipe(print())
         .pipe(shell([
-            'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true -dColorImageResolution=150 -dNOPAUSE  -dBATCH -sOutputFile=_build/<%= f(file.relative) %> _build/<%= file.relative %>',
+            //optimize pdf and improve compatibility. check http://www.tjansson.dk/2012/04/compressing-pdfs-using-ghostscript-under-linux/
+            'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true -dColorImageResolution=150 -dNOPAUSE -dPDFSETTINGS=/printer -dQUIET -dBATCH -sOutputFile=_build/<%= file.relative.replace(".pdf",".tmp") %> _build/<%= file.relative %>',
+            //remove old .pdf and rename .tmp to .pdf
             'rm _build/<%= file.relative %>',
-            'mv _build/<%= f(file.relative) %> _build/<%= file.relative %>'
+            'mv _build/<%= file.relative.replace(".pdf",".tmp") %> _build/<%= file.relative %>'
         ], {
-            templateData: {
-                f: function(s) {
-                    return ("" + s).split('.pdf').join('.tmp')
-                }
-            },
             verbose: false,
             quiet: true
         }))
 })
 
+// delete _input directory
 gulp.task('clean', function() {
     return gulp.src('_build', {
             read: false
@@ -51,12 +49,14 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
 
-gulp.task('watch', function() {
-    gulp.watch('_input/*.md', ['pdf']);
-});
 
-//build and exit
+// build and exit
 gulp.task('build', ['pdf', 'compress']);
 
-//watch for changes
+// watch markdown for changes 
+gulp.task('watch', function() {
+    gulp.watch('_input/*.md', ['build']);
+});
+
+// watch for changes
 gulp.task('default', ['pdf', 'watch', 'compress']);
